9d2b561d16f6d58324670e80c19344e09ccb5801
MrWoodsman <62749434+MrWoodsman@users.noreply.github.com>
2021-10-16 19:01:27 +0200

    Update 3
    - poprawki media queres
    - Dodane czcionki

--- assets/javaScript/main.js
+++ assets/javaScript/main.js
@@ -1,5 +1,10 @@
-function welcome() {
+function start() {
     const span = document.getElementById('user_name');
 
     span.innerHTML = "Mikolaj"
+
+    var arr = document.getElementsByClassName("notification");
+    for (var i = 0; i < arr.length; i++) {
+    arr[i].innerHTML = "!";
+}
 }
